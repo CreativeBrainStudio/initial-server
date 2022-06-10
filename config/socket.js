afafd9cd414f5975a45aae379f4b3cf0101cabca
CreativeBrainStudio <102801479+CreativeBrainStudio@users.noreply.github.com>
2022-06-10 20:36:00 +0800

    Update socket.js

--- config/socket.js
+++ config/socket.js
@@ -2,9 +2,34 @@ const socket = io => {
   io.on("connection", socket => {
     console.log(`connection established by: ${socket.id}`);
 
-    socket.on("send_children", () => {
-      socket.broadcast.emit("receive_children");
+    // Streaming
+    socket.emit("me", socket.id);
+
+    socket.on("leaveCall", () => socket.broadcast.emit("callEnded"));
+
+    socket.on("callUser", data =>
+      io.to(data.userToCall).emit("callUser", {
+        signal: data.signalData,
+        from: data.from,
+        name: data.name,
+      })
+    );
+
+    socket.on("answerCall", data => {
+      io.to(data.to).emit("callAccepted", data.signal);
     });
+
+    // Broadcasting
+    socket.on("broadcast_stream", status =>
+      socket.broadcast.emit("broadcast_status", status)
+    );
+
+    socket.on("load_broadcast", () =>
+      socket.broadcast.emit("get_broadcast_status")
+    );
+
+    // Children collection
+    socket.on("send_children", () => socket.broadcast.emit("receive_children"));
   });
 };
 
