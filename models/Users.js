7ee82bec45625b5fef44a25a711469e30692e823
CreativeBrainStudio <102801479+CreativeBrainStudio@users.noreply.github.com>
2022-06-11 19:33:33 +0800

    user
    carlo

--- models/Users.js
+++ models/Users.js
@@ -3,6 +3,12 @@ const mongoose = require("mongoose"),
 
 const userSchema = new mongoose.Schema(
   {
+    fullName: {},
+    address: {},
+    mobile: {
+      type: String,
+      minlength: 10,
+    },
     email: {
       type: String,
       unique: true,
